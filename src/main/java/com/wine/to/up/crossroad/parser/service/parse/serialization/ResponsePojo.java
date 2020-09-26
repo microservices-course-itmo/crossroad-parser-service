f479441b533e00325ceaf667263ed775af76c668
Konstantin Mosunov <kmosunoff@mail.ru>
2020-09-26 23:12:49 +0300

    Create json receiving method and a corresponding pojo class

---
+++ src/main/java/com/wine/to/up/crossroad/parser/service/parse/serialization/ResponsePojo.java
@@ -0,0 +1,9 @@
+package com.wine.to.up.crossroad.parser.service.parse.serialization;
+
+import java.util.List;
+
+public class ResponsePojo {
+    public int count;
+    public String html;
+    public List<String> jsonTags;
+}
