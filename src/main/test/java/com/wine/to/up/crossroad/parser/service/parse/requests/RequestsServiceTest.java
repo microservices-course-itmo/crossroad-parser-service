3cbe017b28e27ac5de51599fa2d61a65dcfdd6ef
Konstantin Mosunov <kmosunoff@mail.ru>
2020-09-27 19:56:03 +0300

    Upgrade RequestsService

--- src/main/test/java/com/wine/to/up/crossroad/parser/service/parse/requests/RequestsServiceTest.java
+++ src/main/test/java/com/wine/to/up/crossroad/parser/service/parse/requests/RequestsServiceTest.java
@@ -1,22 +1,36 @@
 package com.wine.to.up.crossroad.parser.service.parse.requests;
 
+import com.wine.to.up.crossroad.parser.service.configuration.ParserConfiguration;
 import com.wine.to.up.crossroad.parser.service.parse.serialization.ResponsePojo;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.PropertySource;
 
 import java.util.Optional;
 
 
 @Slf4j
 @SpringBootTest(classes = RequestsService.class)
+@PropertySource("classpath:crossroad-site.properties")
 public class RequestsServiceTest {
+    private RequestsService requestsService;
+
+    @Before
+    public void init() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(ParserConfiguration.class);
+        requestsService = (RequestsService) context.getBean("requestsService");
+    }
+
 
     @Test
     public void testDeserialization() {
-        Optional<ResponsePojo> pojo = RequestsService.getJson("https://www.perekrestok.ru/catalog/alkogol/vino?ajax=true");
+        Optional<ResponsePojo> pojo = requestsService.getJson(1, true);
         Assert.assertTrue(pojo.isPresent());
-        log.info(pojo.get().toString());
+        log.debug(String.valueOf(pojo.get().getCount()));
     }
 }
