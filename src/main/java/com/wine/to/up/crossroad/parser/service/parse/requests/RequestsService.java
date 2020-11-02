4afd80b9ad1b7e07b139f1fad3906c6112f22640
Konstantin Mosunov <kmosunoff@mail.ru>
2020-11-02 13:39:12 +0300

    Send requests via Apache HttpClient, set retry

--- src/main/java/com/wine/to/up/crossroad/parser/service/parse/requests/RequestsService.java
+++ src/main/java/com/wine/to/up/crossroad/parser/service/parse/requests/RequestsService.java
@@ -6,8 +6,16 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
-import org.jsoup.Jsoup;
-import org.springframework.retry.annotation.Retryable;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
+import org.apache.http.impl.client.HttpClientBuilder;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Optional;
 
 @Slf4j
@@ -29,15 +37,32 @@ public class RequestsService {
         this.region = region;
     }
 
-    @Retryable(value = Exception.class) // retries up to three times
     private String getByUrl(String url) throws Exception {
-        return Jsoup.connect(url)
-                .userAgent(userAgent)
-                .timeout(timeout)
-                .data("region", region)
-                .ignoreContentType(true)
-                .execute()
-                .body();
+        RequestConfig config = RequestConfig.custom()
+                .setConnectTimeout(timeout)
+                .setSocketTimeout(timeout)
+                .setConnectionRequestTimeout(timeout)
+                .build();
+        HttpClient client = HttpClientBuilder.create()
+                .setDefaultRequestConfig(config)
+                .setRetryHandler(new DefaultHttpRequestRetryHandler(4, false))
+                .build();
+
+        URIBuilder builder = new URIBuilder(url);
+        builder.setParameter("region", region);
+        HttpGet request = new HttpGet(builder.build());
+
+        request.setHeader(HttpHeaders.USER_AGENT, userAgent);
+        HttpResponse response = client.execute(request);
+
+        BufferedReader rd = new BufferedReader(new InputStreamReader(
+                response.getEntity().getContent()));
+        StringBuilder result = new StringBuilder();
+        String line = "";
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        return result.toString();
     }
 
     public Optional<CatalogResponsePojo> getJson(int page) {
